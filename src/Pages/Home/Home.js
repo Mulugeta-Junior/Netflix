3c63d8e4de8b796d37a90e08637bc98c9f74d2e9
Mulugeta-Junior <mulugeta.kassa2112@gmail.com>
2024-11-08 00:10:42 +0300

    Header & Footer updated

---
+++ src/Pages/Home/Home.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Header from '../../componenets/Header/Header';
+import Footer from '../../componenets/Footer/Footer';
+import Banner from '../../componenets/Banner/Banner';
+
+const Home = () => {
+  return (
+    <>
+      <Header />
+      <Banner />
+      <Footer />
+    </>
+  );
+}
+
+export default Home;
