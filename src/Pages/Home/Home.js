7b4e2d2ab99aa920c0c8aad9473dab08c8ef8d35
Mulugeta-Junior <mulugeta.kassa2112@gmail.com>
2024-11-09 23:39:09 +0300

    row

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,16 +1,18 @@
-import React from 'react'
-import Header from '../../componenets/Header/Header';
-import Footer from '../../componenets/Footer/Footer';
-import Banner from '../../componenets/Banner/Banner';
+import React from "react";
+import Header from "../../componenets/Header/Header";
+import Footer from "../../componenets/Footer/Footer";
+import Banner from "../../componenets/Banner/Banner";
+import RowList from "../../componenets/Row/Rowlist/RowList";
 
 const Home = () => {
   return (
     <>
       <Header />
       <Banner />
+      <RowList />
       <Footer />
     </>
   );
-}
+};
 
 export default Home;
